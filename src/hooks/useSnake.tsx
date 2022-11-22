8c2d2d5cb6c4beb523dbea30a16379e3b6d9d29c
acm-97 <alejandro97.acm@gmail.com>
2022-11-22 12:21:39 +0100

    update  paths

--- src/hooks/useSnake.tsx
+++ src/hooks/useSnake.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
 
-import { getRandomCoordinates } from '@/utils';
+import { getRandomCoordinates } from '../utils';
 
 type useSnakeProps = {
   _speed?: number;
